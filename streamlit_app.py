b879cdf2bd351256cece67b7aabf08330a71caf8
rakesh <79210765+rakeshbongi01@users.noreply.github.com>
2026-09-14 23:30:18 +0530

    YouTube-RAG-QA

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
